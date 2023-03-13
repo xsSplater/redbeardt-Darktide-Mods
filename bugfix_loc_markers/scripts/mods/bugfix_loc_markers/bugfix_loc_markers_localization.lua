994138f20a18710407f2303ec287a80e098d9e01
deluxghost <deluxghost@users.noreply.github.com>
2023-03-13 19:51:12 +0800

    update zh-cn

--- bugfix_loc_markers/scripts/mods/bugfix_loc_markers/bugfix_loc_markers_localization.lua
+++ bugfix_loc_markers/scripts/mods/bugfix_loc_markers/bugfix_loc_markers_localization.lua
@@ -1,5 +1,6 @@
 return {
 	mod_description = {
 		en = "Fixes the location markers players can place ingame so that the displayed colour of the marker reflects the placing player's squad colour.",
+		["zh-cn"] = "修复玩家在游戏内放置的地点标记，使标记颜色符合放置标记玩家的小队颜色。",
 	}
 }
--- holier_revenant/scripts/mods/holier_revenant/holier_revenant_localization.lua
+++ holier_revenant/scripts/mods/holier_revenant/holier_revenant_localization.lua
@@ -1,11 +1,14 @@
 return {
 	mod_description = {
-		en = "Adds an icon like your class ability icon, but it shows the status of your Holy Revenant buff."
+		en = "Adds an icon like your class ability icon, but it shows the status of your Holy Revenant buff.",
+		["zh-cn"] = "添加一个狂信徒镰刀死神被动能力的状态图标，类似角色主动能力。",
 	},
 	gradual_background_update = {
-		en = "Gradual Background Update"
+		en = "Gradual Background Update",
+		["zh-cn"] = "动态更新背景",
 	},
 	gradual_background_update_description = {
-		en = "Causes the icon to update its background colour gradually and vertically as its cooldown progresses."
+		en = "Causes the icon to update its background colour gradually and vertically as its cooldown progresses.",
+		["zh-cn"] = "随着能力冷却进度，逐渐在垂直方向更新图标背景颜色。",
 	}
 }
