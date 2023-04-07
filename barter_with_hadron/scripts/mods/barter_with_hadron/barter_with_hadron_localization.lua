ce8efa2009cbf619d462e967560ea8efb8e4ca8f
xsSplater <xssplater@gmail.com>
2023-04-07 12:35:40 +0300

    Added the possibility of localization of the name (#7)
    and added localization itself.
    
    Co-authored-by: redbeardt <redbeardt@posteo.me>

--- barter_with_hadron/scripts/mods/barter_with_hadron/barter_with_hadron_localization.lua
+++ barter_with_hadron/scripts/mods/barter_with_hadron/barter_with_hadron_localization.lua
@@ -1,6 +1,11 @@
 return {
+	mod_name = {
+		en = "Barter with Hadron",
+		ru = "Бартер с Хадрон",
+	},
 	mod_description = {
 		en = "Gives the player the ability to barter items while working with Hadron.",
-		["zh-cn"] = "玩家可以在海德昂锻造界面交易物品。",
+		ru = "Дает игроку возможность продавать предметы при работе с Хадрон.",
+		["zh-cn"] = "玩家可以在海德昂锻造界面交易物品。"
 	}
 }
