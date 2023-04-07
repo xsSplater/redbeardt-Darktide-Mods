b3f2e50a57ff5a4be60ae1fa5495a5e39d34ed04
xsSplater <xssplater@gmail.com>
2023-04-07 12:34:46 +0300

    Added the possibility of localization of the name. (#6)

--- barter_with_hadron/scripts/mods/barter_with_hadron/barter_with_hadron_data.lua
+++ barter_with_hadron/scripts/mods/barter_with_hadron/barter_with_hadron_data.lua
@@ -1,7 +1,7 @@
 local mod = get_mod("barter_with_hadron")
 
 return {
-	name = "Barter with Hadron",
+	name = mod:localize("mod_name"),
 	description = mod:localize("mod_description"),
 	is_togglable = true
 }
