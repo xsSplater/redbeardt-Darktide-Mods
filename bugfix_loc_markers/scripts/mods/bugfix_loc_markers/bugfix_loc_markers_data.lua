9ac3814f4edfd2af1a9d6d394ba0bedc99b1b5bb
xsSplater <xssplater@gmail.com>
2023-04-07 12:37:36 +0300

    Added the possibility of localization of the name. (#9)

--- bugfix_loc_markers/scripts/mods/bugfix_loc_markers/bugfix_loc_markers_data.lua
+++ bugfix_loc_markers/scripts/mods/bugfix_loc_markers/bugfix_loc_markers_data.lua
@@ -1,7 +1,7 @@
 local mod = get_mod("bugfix_loc_markers")
 
 return {
-	name = "Bugfix: Location Marker Colours",
+	name = mod:localize("mod_name"),
 	description = mod:localize("mod_description"),
 	is_togglable = true
 }
