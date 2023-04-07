18abc9db2556a9b71631bbc611f5feada3e3b6e8
xsSplater <xssplater@gmail.com>
2023-04-07 12:37:14 +0300

    Added the possibility of localization of the name (#8)
    and localization itself.
    
    Co-authored-by: redbeardt <redbeardt@posteo.me>

--- bugfix_loc_markers/scripts/mods/bugfix_loc_markers/bugfix_loc_markers_localization.lua
+++ bugfix_loc_markers/scripts/mods/bugfix_loc_markers/bugfix_loc_markers_localization.lua
@@ -1,6 +1,11 @@
 return {
+	mod_name = {
+		en = "Bugfix: Location Marker Colours",
+		ru = "Исправление: цвет маркеров местоположения",
+	},
 	mod_description = {
 		en = "Fixes the location markers players can place ingame so that the displayed colour of the marker reflects the placing player's squad colour.",
-		["zh-cn"] = "修复玩家在游戏内放置的地点标记，使标记颜色符合放置标记玩家的小队颜色。",
+		ru = "Исправляет маркеры, которые игроки размещают в игре так, чтобы отображаемый цвет маркера соответствовал цвету игрока, размещающего его.",
+		["zh-cn"] = "修复玩家在游戏内放置的地点标记，使标记颜色符合放置标记玩家的小队颜色。"
 	}
 }
