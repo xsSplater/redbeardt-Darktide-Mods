4ae67eaa966ac6cc4dd91e76366b4fd62b9d477a
xsSplater <xssplater@gmail.com>
2023-04-07 12:38:29 +0300

    Update no_skull_localization.lua (#11)

--- no_skull/scripts/mods/no_skull/no_skull_localization.lua
+++ no_skull/scripts/mods/no_skull/no_skull_localization.lua
@@ -1,6 +1,11 @@
 return {
+	mod_name = {
+		en = "No Skull",
+		ru = "Без черепа",
+	},
 	mod_description = {
 		en = "Removes the skull icon from threat-tagging because sometimes it gets in the way.",
+		ru = "Убирает красный череп с отмеченного врага, не убирая красной обводки, чтобы было удобнее в него целиться, особенно издалека.",
 		["zh-cn"] = "从威胁标记中删除骷髅图标，因为有时它们会遮挡画面。",
 	}
 }
