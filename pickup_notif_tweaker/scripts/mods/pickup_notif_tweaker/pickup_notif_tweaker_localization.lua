33f6975368a626ce52c338e5723e805bff84b30b
xsSplater <xssplater@gmail.com>
2023-04-10 10:49:57 +0300

    Update pickup_notif_tweaker_localization.lua

--- pickup_notif_tweaker/scripts/mods/pickup_notif_tweaker/pickup_notif_tweaker_localization.lua
+++ pickup_notif_tweaker/scripts/mods/pickup_notif_tweaker/pickup_notif_tweaker_localization.lua
@@ -2,89 +2,111 @@ return {
 	mod_name = {
 		en = "Pickup Notifications Tweaker",
 		["zh-cn"] = "拾取通知优化",
+		ru = "Настройка уведомлений о поднятии предметов",
 	},
 	mod_description = {
 		en = "Gives some control over the material pickup notification system.",
 		["zh-cn"] = "控制材料拾取通知系统的行为。",
+		ru = "Pickup Notifications Tweaker - Даёт некоторый контроль над системой уведомлений о получении материалов для кузницы.",
 	},
 	show_text = {
 		en = "Show Text",
 		["zh-cn"] = "显示文本",
+		ru = "Показывать текст",
 	},
 	show_text_description = {
 		en = "Toggles display of any text in pickup notifications.",
 		["zh-cn"] = "开关拾取通知的文本显示。",
+		ru = "Переключает отображение любого текста в уведомлениях о поднятии предметов.",
 	},
 	show_icons = {
 		en = "Show Icons",
 		["zh-cn"] = "显示图标",
+		ru = "Показывать значки",
 	},
 	show_icons_description = {
 		en = "Toggles display of the material icon in pickup notifications.",
 		["zh-cn"] = "开关拾取通知的材料图标显示。",
+		ru = "Переключает отображение значков материалов в уведомлениях о поднятии предметов.",
 	},
 	show_background = {
 		en = "Show Background",
 		["zh-cn"] = "显示背景",
+		ru = "Показывать фон",
 	},
 	show_background_description = {
 		en = "Toggles display of the pickup notification background.",
 		["zh-cn"] = "开关拾取通知的背景显示。",
+		ru = "Переключает отображение фона уведомлений.",
 	},
 	play_sounds = {
 		en = "Play Sounds",
 		["zh-cn"] = "播放音效",
+		ru = "Воспроизводить звуки",
 	},
 	play_sounds_description = {
 		en = "Toggles playing of sounds as part of pickup notifications.",
 		["zh-cn"] = "开关拾取通知时的音效。",
+		ru = "Переключает воспроизведение звука как часть уведомлений о поднятии предметов.",
 	},
 	scale = {
 		en = "Scale (%%)",
 		["zh-cn"] = "缩放（%%）",
+		ru = "Масштаб (%%)",
 	},
 	scale_description = {
 		en = "Sets visual scale of the notification proportional to its default size, using a percentage value.",
 		["zh-cn"] = "以百分比形式，设置通知框相对其默认大小的视觉缩放比例。",
+		ru = "Устанавливает визуальный масштаб уведомлений, пропорциональный их размеру по умолчанию, используя процентное соотношение.",
 	},
 	use_custom_background_colour = {
 		en = "Use Custom Background Colour",
 		["zh-cn"] = "自定义背景色",
+		ru = "Использовать пользовательский цвет фона",
 	},
 	use_custom_background_colour_description = {
 		en = "Will enable display of the background of the pickup notification using the ARGB colour specified by the following four settings.",
 		["zh-cn"] = "启用自定义拾取通知的背景颜色，其 ARGB 颜色值由以下四个设置组成。",
+		ru = "Включает отображение фона уведомлений о поднятии предметов с использованием цвета, заданного следующими четырьмя настройками.",
 	},
 	custom_background_alpha = {
 		en = "   Alpha",
 		["zh-cn"] = "   透明度",
+		ru = "   Прозрачность",
 	},
 	custom_background_alpha_description = {
 		en = "Alpha (opacity) component of the custom background colour. The lower this number, the more transparent the background will be.",
 		["zh-cn"] = "自定义背景色透明度通道。数字越小，背景越透明。",
+		ru = "Альфа-компонент (прозрачность) пользовательского цвета фона. Чем меньше это число, тем более прозрачным будет фон.",
 	},
 	custom_background_red = {
 		en = "   Red",
 		["zh-cn"] = "   红",
+		ru = "   Красный",
 	},
 	custom_background_red_description = {
 		en = "Red component of the custom background colour. The higher this is, the more red the background will be.",
 		["zh-cn"] = "自定义背景色红色通道。数字越大，背景越红。",
+		ru = "Чем выше это значение, тем больше красного оттенка будет в цвете фона.",
 	},
 	custom_background_green = {
 		en = "   Green",
 		["zh-cn"] = "   绿",
+		ru = "   Зелёный",
 	},
 	custom_background_green_description = {
 		en = "Green component of the custom background colour. The higher this is, the more green the background will be.",
 		["zh-cn"] = "自定义背景色绿色通道。数字越大，背景越绿。",
+		ru = "Чем выше это значение, тем больше зелёного оттенка будет в цвете фона.",
 	},
 	custom_background_blue = {
 		en = "   Blue",
 		["zh-cn"] = "   蓝",
+		ru = "   Синий",
 	},
 	custom_background_blue_description = {
 		en = "Blue component of the custom background colour. The higher this is, the more blue the background will be.",
 		["zh-cn"] = "自定义背景色蓝色通道。数字越大，背景越蓝。",
+		ru = "Чем выше это значение, тем больше синего оттенка будет в цвете фона.",
 	}
 }
