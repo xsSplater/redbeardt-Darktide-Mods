c9cf5e32539c2d96de2fef5d91e5e6aae6a2fd5e
xsSplater <xssplater@gmail.com>
2023-04-10 10:28:43 +0300

    Update holier_revenant_localization.lua

--- holier_revenant/scripts/mods/holier_revenant/holier_revenant_localization.lua
+++ holier_revenant/scripts/mods/holier_revenant/holier_revenant_localization.lua
@@ -2,17 +2,21 @@ return {
 	mod_name = {
 		en = "Holier Revenant",
 		["zh-cn"] = "镰刀死神",
+		ru = "Священнейший призрак",
 	},
 	mod_description = {
 		en = "Adds an icon like your class ability icon, but it shows the status of your Holy Revenant buff.",
 		["zh-cn"] = "添加一个狂信徒镰刀死神被动能力的状态图标，类似角色主动能力。",
+		ru = "Holier Revenant - Добавляет значок, похожий на значок способности Изувера, но он показывает статус таланта Священный призрак.",
 	},
 	gradual_background_update = {
 		en = "Gradual Background Update",
 		["zh-cn"] = "动态更新背景",
+		ru = "Постепенное обновление фона",
 	},
 	gradual_background_update_description = {
 		en = "Causes the icon to update its background colour gradually and vertically as its cooldown progresses.",
 		["zh-cn"] = "随着能力冷却进度，逐渐在垂直方向更新图标背景颜色。",
+		ru = "Заставляет значок постепенно обновлять цвет фона вверх по вертикали по мере перезарядки.",
 	}
 }
